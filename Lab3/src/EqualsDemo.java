155830a5875f67f5107ed8a3bd523cd9c1fbe85f
NataliaDanci <nataliadanci0@gmail.com>
2024-04-29 18:33:09 +0300

    Added student's cnp

--- Lab3/src/EqualsDemo.java
+++ Lab3/src/EqualsDemo.java
@@ -1,8 +1,9 @@
 public class EqualsDemo {
     public static void main(String[] args) {
-        Student student1 = new Student("Cristina", 10,234);
-        Student student2 = new Student("Daniel", 18,567);
-        Student student3 = new Student("Cristina", 10,890);
+
+        Student student1 = new Student("Cristina", 10, 9, "12345678");
+        Student student2 = new Student("Daniel", 18,7,"98765432");
+        Student student3 = new Student("Cristina", 10, 9,"11111111");
 
         if(student1==student3){
             System.out.println("student1==student3");
--- Lab3/src/Student.java
+++ Lab3/src/Student.java
@@ -1,20 +1,22 @@
 public class Student {
     private String name;
     private int grade;
-
     private int id;
+    private String cnp;
 
-    public Student(String name, int grade, int id){
+    public Student(String name, int grade, int id, String cnp){
         this.name = name;
         this.grade = grade;
         this.id = id;
+        this.cnp = cnp;
     }
 
     public boolean equals(Object object){
         // clasa Object este clasa parinte pt toate clasele, inclusiv pt Student
         Student student = (Student)  object;
         return this.name == student.name &&
+                this.grade == student.grade &&
                 this.id == student.id &&
-                this.grade == student.grade;
+                this.cnp == student.cnp;
     }
 }
