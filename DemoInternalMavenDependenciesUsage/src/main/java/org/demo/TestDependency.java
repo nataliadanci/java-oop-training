459d10469e5d63a3f16a0c9bbfe18f00965f5469
NataliaDanci <nataliadanci0@gmail.com>
2024-09-03 18:56:42 +0300

    Added Maven project example for dependency usage     -example on how to use previously generated JAR(from a Maven project) in a pom.xml file of another Maven project

---
+++ DemoInternalMavenDependenciesUsage/src/main/java/org/demo/TestDependency.java
@@ -0,0 +1,9 @@
+package org.demo;
+
+import org.example.GsonTester;
+
+public class TestDependency {
+    public static void main(String[] args) {
+        GsonTester.main(new String[]{});
+    }
+}
